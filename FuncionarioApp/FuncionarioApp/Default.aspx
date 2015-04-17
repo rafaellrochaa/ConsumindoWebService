624badb2cc90f02358aa36da991a12eb4dbbb6bb
Rafael <rafaell.rochaa@gmail.com>
2015-04-17 18:31:22 -0300

    Commit de consumo do webservice

---
+++ FuncionarioApp/FuncionarioApp/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title=Home Language=C# MasterPageFile=~/Site.master AutoEventWireup=true CodeBehind=Default.aspx.cs Inherits=FuncionarioApp._Default %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <fieldset>
+        <legend>Pesquisar Funcionário</legend>
+        <table>
+            <tr>
+                <td>Sexo </td>
+                <td>
+                    <asp:DropDownList ID="ddlSexo" runat="server">
+                        <asp:ListItem Value="M">Masculino</asp:ListItem>
+                        <asp:ListItem Value="F">Feminino</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+                <td>
+                    <asp:Button ID="btnPesquisar" runat="server" OnClick="btnPesquisar_Click" Text="Pesquisar" />
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <br />
+    <div>
+        <asp:GridView ID="gvwFuncionarios" runat="server" AutoGenerateColumns="False" Width="100%">
+            <Columns>
+                <asp:BoundField HeaderText="ID" DataField="idt_func" />
+                <asp:BoundField HeaderText="CPF" DataField="cpf_func" />
+                <asp:BoundField HeaderText="Nome" DataField="nom_func" />
+                <asp:BoundField HeaderText="RG" DataField="ci_func" />
+                <asp:BoundField HeaderText="Email" DataField="email_func" />
+                <asp:BoundField HeaderText="Tel." DataField="tel_res_func" />
+                <asp:BoundField HeaderText="Tel." DataField="tel_cel_func" />
+                <asp:BoundField HeaderText="Data" DataField="dat_nasc_func" />
+                <asp:BoundField HeaderText="Sexo" DataField="sexo_func" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
+
+
+
---
+++ FuncionarioApp/FuncionarioApp/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Web; 
+using System.Web.UI; 
+using System.Web.UI.WebControls; 
+using System.Data;
+
+namespace FuncionarioApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            //Instanciando o nosso web service 
+            //FuncionarioApp.localhost.FuncionariosSoapClient ws = new ServiceReference1.FuncionariosSoapClient();
+            FuncionarioApp.localhost.Funcionarios ws= new localhost.Funcionarios();
+            
+            //Chamando o método ObterFuncionariosPorSexo do nosso web service passando o sexo como parâmetro 
+            //ServiceReference1.xml obj = ws.ObterFuncionariosPorSexo(ddlSexo.SelectedValue);
+            FuncionarioApp.localhost.xml obj = ws.ObterFuncionariosPorSexo(ddlSexo.SelectedValue);
+            //Criando DataTable que conterá a informação dos funcionários 
+            DataTable dteFunc = new DataTable();
+            dteFunc.Columns.Add("idt_func");
+            dteFunc.Columns.Add("cpf_func");
+            dteFunc.Columns.Add("nom_func");
+            dteFunc.Columns.Add("ci_func");
+            dteFunc.Columns.Add("email_func");
+            dteFunc.Columns.Add("tel_res_func");
+            dteFunc.Columns.Add("tel_cel_func");
+            dteFunc.Columns.Add("dat_nasc_func");
+            dteFunc.Columns.Add("sexo_func");
+            //Percorrendo o objeto retornado pelo nosso web service e adicionando ao DataTable 
+            for (int i = 0; i < obj.funcionarios.Length; i++)
+            {
+                dteFunc.Rows.Add(
+                    obj.funcionarios[i].idt_func,
+                    obj.funcionarios[i].cpf,
+                    obj.funcionarios[i].nome,
+                    obj.funcionarios[i].rg,
+                    obj.funcionarios[i].email,
+                    obj.funcionarios[i].tel_residencial,
+                    obj.funcionarios[i].tel_celular,
+                    obj.funcionarios[i].data_nascimento,
+                    obj.funcionarios[i].sexo);
+            }
+            //Vinculando o DataTable ja preenchido ao GridView
+            gvwFuncionarios.DataSource = dteFunc;
+            gvwFuncionarios.DataBind();
+        }
+    }
+}
